7c3e5eb623b1bced19b936aa7c9d82427d9510bb
febrian syahroni <febrian.fs716@gmail.com>
2024-08-14 08:36:15 +0700

    coba lagi

--- index.js
+++ index.js
@@ -79,3 +79,8 @@ app.get("/contact", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server berjalan di port 3000");
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
